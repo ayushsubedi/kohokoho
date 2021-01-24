2ceb6cb73505de0d53e90f5781895cbf810a30a1
Ayush Subedi <ayush.subedi@gmail.com>
2021-01-24 21:28:58 +0545

    resolve #18

--- kohokoho.py
+++ kohokoho.py
@@ -1,4 +1,5 @@
 import click
+import random
 import pandas as pd
 from faker import Faker
 
@@ -18,6 +19,13 @@ class anon(object):
         map_dict = {_: fake.uuid4() for _ in unique}
         self.df[col] = self.df[col].map(map_dict)
 
+    def anon_whole_num(self, col):
+        X_std = (self.df[col] - self.df[col].min()) / (self.df[col].max() - self.df[col].min())
+        X_scaled = (X_std * (10 - 1) + 1) 
+        X_scaled_whole_randomized = (X_scaled * random.randint(1, 10)).astype(int)
+        self.df[col] = X_scaled_whole_randomized
+
+
     def anon_df(self):
         return self.df
 
@@ -30,12 +38,16 @@ class anon(object):
 def cli(csv):
     df = pd.read_csv(csv)
     koho_df = anon(df)
-    click.echo('Column names: '+str(list(df)))
+    click.echo('Columns info: '+str(df.info()))
     name_col = click.prompt('Enter column which stores names', type=str)
     koho_df.anon_name(name_col)
     
     id_col = click.prompt('Enter column which stores id', type=str)
     koho_df.anon_id(id_col)
+
+    whole_col = click.prompt('Enter column which stores whole numbers', type=str)
+    koho_df.anon_whole_num(whole_col)
+
     click.echo(koho_df.anon_df().head())
     
 
